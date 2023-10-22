2588e126d26996b050bce945e9c8b203b7489f36
Pablo Cabrejos <84685388+Bosnape@users.noreply.github.com>
2023-10-21 21:14:30 -0500

    Update Torre.java

--- Torre.java
+++ Torre.java
@@ -1,15 +1,15 @@
 public class Torre {
-    private int cantidad_p = 12;
-    private int[] parq = new int[cantidad_p];
-    private String lugar = "None";
+    private int[] parq = new int[12];
+    private String lugar;
     private String nombre;
 
-    public Torre(String nombre, int cantidad_p, String lugar){
-        set_nombre(nombre);
-        set_t_parqueaderos(cantidad_p);
-        set_lugar(lugar);
+    // Constructor
+    public Torre(String nombre, String lugar){
+        this.nombre = nombre;
+        this.lugar = lugar;
     }
 
+    // Acceder o manipular el atributo lugar
     public String get_lugar(){
         return lugar;
     }
@@ -17,13 +17,7 @@ public class Torre {
         this.lugar = l;
     }
 
-    public int get_t_parqueaderos(){
-        return cantidad_p;
-    }
-    public void set_t_parqueaderos(int p){
-        this.cantidad_p = p;
-    }
-
+    // Acceder o manipular el atributo nombre
     public String get_nombre(){
         return nombre;
     }
@@ -31,30 +25,38 @@ public class Torre {
         this.nombre = n;
     }
 
+    // Acceder al atributo cantidad de parqueaderos
+    public int cantidad_parqueaderos() {
+        return parq.length;
+    }
+
+    // Revisar si hay parqueaderos libres
     public boolean libre(){
-        int espacios_l = 0;
         for (int espacio = 0; espacio < parq.length; espacio++){
             if (parq[espacio] == 0){
-                espacios_l += 1;
+                return true;
             }
         }
-        return espacios_l > 0;
+        return false;
     }
-    public void actualizar(int n){
-        if (n == 1){
-            for (int espacio = 0; espacio < parq.length; espacio++){
-                if (parq[espacio] == 0){
-                    parq[espacio] = 1;
-                }
+
+    // Ocupar un parqueadero
+    public void ocupar(){
+        for (int espacio = 0; espacio < parq.length; espacio++){
+            if (parq[espacio] == 0){
+                parq[espacio] = 1;
+                break;
             }
-        } else if (n == 0){
-            for (int espacio = 0; espacio < parq.length; espacio++){
-                if (parq[espacio] == 1){
-                    parq[espacio] = 0;
-                }
-            }
-        } else {
-            System.out.println("Opciones: 0 | 1");
         }
+    }
+
+    public int parq_libres(){
+        int libres = 0;
+        for (int espacio = 0; espacio < parq.length; espacio++){
+            if (parq[espacio] == 0){
+                libres++;
+            }
+        }
+        return libres;
     }
 }
