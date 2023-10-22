3acbcf3428a8b5c9493441f3da2fdcc11e5740eb
Pablo Cabrejos <84685388+Bosnape@users.noreply.github.com>
2023-10-21 21:13:39 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,95 +1,270 @@
 import java.util.Scanner;
-import java.util.*;
+
 public class Main {
 
-    public static Torre torre1 = new Torre("Torre 1", 12, "Parqueadero 1");
-    public static Torre torre2 = new Torre("Torre 2", 12, "Parqueadero 2");
-    public static Torre torre3 = new Torre("Torre 3", 12, "Parqueadero 3");
+    public static Torre torre1 = new Torre("Torre 1", "cerca de la cancha norte");
+    public static Torre torre2 = new Torre("Torre 2", "en el parqueadero de los profesores");
+    public static Torre torre3 = new Torre("Torre 3", "cerca del bloque 19");
 
-    public static void cancelar_parqueadero(int v){
-        Scanner in = new Scanner(System.in);
-        int valor = v;
-        int saldo = 0;
+    public static Database usuarios = new Database("C:\\Users\\Pcabr\\IdeaProjects\\Vertical Parking\\usuarios.txt");
+    public static Database entrada_salida = new Database("C:\\Users\\Pcabr\\IdeaProjects\\Vertical Parking\\entrada_salida.txt");
+    public static Database billeteras = new Database("C:\\Users\\Pcabr\\IdeaProjects\\Vertical Parking\\billeteras.txt");
 
-        System.out.println("--- Saldo: " + saldo + "$ ---\n");
-        System.out.println("Valor a pagar: " + valor);
-        System.out.print("Ingrese el dinero: ");
-        valor -= in.nextInt();
-
-        while (valor > 0){
-            System.out.print("Falta $" + valor + ", inregese el valor faltante: ");
-            valor -= in.nextInt();
+    public static boolean placa_valida(String placa){
+        if (placa.length() == 6){
+            for (int i = 0; i < placa.length(); i++){
+                if (i < 3){
+                    if (!Character.isLetter(placa.charAt(i))){
+                        return false;
+                    }
+                } else {
+                    if (!Character.isDigit(placa.charAt(i))){
+                        return false;
+                    }
+                }
+            }
+            return true;
+        } else {
+            return false;
         }
+    }
 
-        saldo = -1 * valor;
-        System.out.println("\nPago confirmado!");
-        System.out.println("Le queda $" + saldo + " a favor en la cuenta.");
-        System.out.println("Buen viaje!");
-
-        in.close();
+    public static boolean nombre_valido(String nombre){
+        if (nombre.length() > 3){
+            for (int i = 0; i < nombre.length(); i++){
+                if (!Character.isLetter(nombre.charAt(i)) && nombre.charAt(i) != ' '){
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public static void main(String[] args){
 
         Scanner in = new Scanner(System.in);
-        System.out.println("Bienvenido al sistema de parqueo vertical!");
-        System.out.println("A continuacion vamos a pedir cierta informacion basica para poder brindar el servicio.");
 
-        // Recolectar datos del usuario
-        System.out.print("Nombre completo: ");
-        String nombre = in.nextLine();
-        System.out.print("Placa del carro: ");
-        String placa = in.nextLine();
-
-        // Gestionar los parqueaderos
-        System.out.println("\nRevisando parqueaderos libres...");;
-        if (torre1.libre() || torre2.libre() || torre3.libre()){
-            System.out.println("Listo " + nombre + ", hay parqueaderos disponibles");
-            System.out.println("Enseguida te indicaremos las torres");
-        } else {
-            System.out.println(nombre + ", por el momento no hay parqueaderos");
-            System.exit(0);
-        }
-
-        // Guiar al usuario
-        String opciones = "| ";
-        if (torre1.libre()){
-            opciones += torre1.get_nombre() + " | ";
-        }
-        if (torre2.libre()){
-            opciones += torre2.get_nombre() + " | ";
-        }
-        if (torre3.libre()) {
-            opciones += torre3.get_nombre() + " | ";
-        }
-        System.out.println("\nLas siguentes torres estan disponibles: \n" + opciones);
-        System.out.print("Elija una de ellas (1|2|3): ");
-        int eleccion = in.nextInt();
-
-        if (eleccion == 1){
-            System.out.println("La Torre " + eleccion + " queda por: " + torre1.get_lugar());
-            torre1.actualizar(1);
-        } else if (eleccion == 2){
-            System.out.println("La Torre " + eleccion + " queda por: " + torre2.get_lugar());
-            torre2.actualizar(1);
-        } else if (eleccion == 3){
-            System.out.println("La Torre " + eleccion + " queda por: " + torre3.get_lugar());
-            torre3.actualizar(1);
-        } else {
-            System.out.println("Elija una opcion valida");
-            System.exit(0);
-        }
-
-        // Generar y cancelar el pago electronico
-        System.out.println("\nTienes pendiente cancelar el parqueadero.");
+        // Tarifa del parqueadero
         int TARIFA = 6000;
-        cancelar_parqueadero(TARIFA);
-        if (eleccion == 1){
-            torre1.actualizar(0);
-        } else if (eleccion == 2){
-            torre2.actualizar(0);
+
+        System.out.println("Sistema de parqueo vertical");
+
+        // Verificar/Recolectar los datos del usuario
+        System.out.print("Ingrese la placa del carro: ");
+        String placa = in.nextLine();
+        while (!placa_valida(placa)){
+            System.out.print("Ingrese una placa valida: ");
+            placa = in.nextLine();
+        }
+        placa = placa.toUpperCase();
+        placa = placa.trim();
+
+        User usuario = usuarios.recolectar_usuario(placa);
+
+        if (usuario != null){
+            System.out.println("Bienvenido de nuevo " + usuario.get_nombre() + "!");
         } else {
-            torre3.actualizar(0);
+            System.out.println("Bienvenido, al parecer es la primera vez que nos visitas.");
+            System.out.print("Por favor ingresa tu nombre completo: ");
+            String nombre = in.nextLine();
+            while (!nombre_valido(nombre)){
+                System.out.print("Ingrese un nombre valido: ");
+                nombre = in.nextLine();
+            }
+            nombre = nombre.trim();
+            if (nombre.indexOf(' ') != -1){
+                String[] nombre_completo = nombre.split(" ");
+                nombre = "";
+                for (int i = 0; i < nombre_completo.length; i++){
+                    if (i == 0){
+                        nombre += nombre_completo[i].substring(0, 1).toUpperCase() + nombre_completo[i].substring(1).toLowerCase();
+                    } else {
+                        nombre += " " + nombre_completo[i].substring(0, 1).toUpperCase() + nombre_completo[i].substring(1).toLowerCase();
+                    }
+                }
+            } else {
+                nombre = nombre.substring(0, 1).toUpperCase() + nombre.substring(1).toLowerCase();
+            }
+            usuario = usuarios.nuevo_usuario(nombre, placa);
+            usuarios.add_user(usuario);
+        }
+
+        // Revisar usuario frecuente y dar descuento
+        if (entrada_salida.usuario_frecuente(usuario.get_id())){
+            TARIFA = (int) (TARIFA * 0.80);
+        }
+
+        // Verificar/Recolectar los datos de la billetera
+        Billetera billetera = billeteras.recolectar_billetera(usuario.get_id());
+        if (billetera == null){
+            billetera = new Billetera(usuario.get_id(), 0);
+            billeteras.add_billetera(billetera);
+        }
+
+        // Verificar si el usuario ya habia ingresado
+        if (entrada_salida.usuario_adentro(usuario.get_id())){
+
+            // Preguntar si el usuario quiere pagar ahora o despues
+            System.out.println("\nTienes pendiente cancelar el parqueadero.");
+            System.out.println("Quieres continuar con el pago?\n1. Si\n2. No");
+            System.out.print("Opcion: ");
+            int opcion = in.nextInt();
+            while (opcion != 1 && opcion != 2){
+                System.out.print("Ingrese una opcion valida: ");
+                opcion = in.nextInt();
+            }
+
+            if (opcion == 2){
+                System.out.println("\nRecuerda cancelar el parqueadero para salir!");
+                System.exit(0);
+            }
+
+            // Generar y mostrar el pago
+            if (billetera.get_saldo() >= TARIFA){
+                System.out.println("\n--- Saldo: " + billetera.get_saldo() + "$ ---");
+
+                // Mostrar el descuento si el usuario es frecuente
+                if (entrada_salida.usuario_frecuente(usuario.get_id())){
+                    System.out.println("DESCUENTO: 20%");
+                }
+                System.out.println("Tarifa: " + TARIFA + "$");
+                billetera.retirar(TARIFA);
+                System.out.println("Pago confirmado!");
+                System.out.println("\nTu saldo actualizado es de " + billetera.get_saldo() + "$");
+
+            } else {
+                System.out.println("\n--- Saldo: " + billetera.get_saldo() + "$ ---");
+
+                // Mostrar el descuento si el usuario es frecuente
+                if (entrada_salida.usuario_frecuente(usuario.get_id())){
+                    System.out.println("DESCUENTO: 20%");
+                }
+                System.out.println("Tarifa: " + TARIFA + "$");
+                int valor = TARIFA - billetera.get_saldo();
+                billetera.retirar(billetera.get_saldo());
+                System.out.println("Valor a pagar: " + valor + "$");
+                System.out.print("Ingrese el dinero: ");
+                valor -= in.nextInt();
+
+                while (valor > 0){
+                    System.out.print("Falta $" + valor + ", inregese el valor faltante: ");
+                    valor -= in.nextInt();
+                }
+
+                System.out.println("Pago confirmado.");
+                System.out.println("\nLe queda $" + (-1 * valor) + " a favor en la cuenta.");
+                billetera.depositar(valor * -1);
+            }
+
+            // Actualizar los datos de la billetera
+            billeteras.actualizar_billetera(billetera);
+
+            // Liberar el parqueadero y finalizar el servicio
+            entrada_salida.add_salida(usuario.get_id());
+            System.out.println("Vuelva pronto!");
+
+        } else {
+
+            // Gestionar los parqueaderos
+            System.out.println("\nRevisando parqueaderos libres...");
+            entrada_salida.actualizar_torres(torre1, torre2, torre3);
+            if (torre1.libre() || torre2.libre() || torre3.libre()) {
+                System.out.println(usuario.get_nombre() + ", hay parqueaderos disponibles!");
+            } else {
+                System.out.println(usuario.get_nombre() + ", por el momento no hay parqueaderos.");
+                System.exit(0);
+            }
+
+            // Guiar al usuario y actualizar el estado de los parqueaderos
+            String opciones = "";
+            if (torre1.libre()) {
+                opciones += "1. Torre 1 \n";
+            }
+            if (torre2.libre()) {
+                opciones += "2. Torre 2 \n";
+            }
+            if (torre3.libre()) {
+                opciones += "3. Torre 3 \n";
+            }
+
+            System.out.println("\nLas siguientes torres estan libres:\n" + opciones);
+            System.out.print("Elija una torre: ");
+            int eleccion = in.nextInt();
+            while (eleccion < 1 || eleccion > 3) {
+                System.out.print("Ingrese una opcion valida: ");
+                eleccion = in.nextInt();
+            }
+
+            if (eleccion == 1) {
+                System.out.println("Ten en cuenta que la " + torre1.get_nombre() + " queda " + torre1.get_lugar());
+            } else if (eleccion == 2) {
+                System.out.println("Ten en cuenta que la " + torre2.get_nombre() + " queda " + torre2.get_lugar());
+            } else if (eleccion == 3) {
+                System.out.println("Ten en cuenta que la " + torre3.get_nombre() + " queda " + torre3.get_lugar());
+            }
+            entrada_salida.add_entrada(usuario.get_id(), eleccion);
+
+            // Preguntar si el usuario quiere pagar ahora o despues
+            System.out.println("\nTienes pendiente cancelar el parqueadero.");
+            System.out.println("Quieres pagar ya?\n1. Si\n2. No");
+            System.out.print("Opcion: ");
+            int opcion = in.nextInt();
+            while (opcion != 1 && opcion != 2) {
+                System.out.print("Ingrese una opcion valida: ");
+                opcion = in.nextInt();
+            }
+
+            if (opcion == 2) {
+                System.out.println("\nGracias por usar el sistema de parqueo vertical!");
+                System.out.println("Recuerda cancelar el parqueadero para salir.");
+                System.exit(0);
+            }
+
+            // Generar y mostrar el pago
+            if (billetera.get_saldo() >= TARIFA) {
+                System.out.println("\n--- Saldo: " + billetera.get_saldo() + "$ ---");
+
+                // Mostrar el descuento si el usuario es frecuente
+                if (entrada_salida.usuario_frecuente(usuario.get_id())){
+                    System.out.println("DESCUENTO: 20%");
+                }
+                System.out.println("Tarifa: " + TARIFA + "$");
+                billetera.retirar(TARIFA);
+                System.out.println("Pago confirmado.");
+                System.out.println("\nTu saldo actualizado es de " + billetera.get_saldo() + "$");
+
+            } else {
+                System.out.println("\n--- Saldo: " + billetera.get_saldo() + "$ ---");
+
+                // Mostrar el descuento si el usuario es frecuente
+                if (entrada_salida.usuario_frecuente(usuario.get_id())){
+                    System.out.println("DESCUENTO: 20%");
+                }
+                System.out.println("Tarifa: " + TARIFA + "$");
+                int valor = TARIFA - billetera.get_saldo();
+                billetera.retirar(billetera.get_saldo());
+                System.out.println("Valor a pagar: " + valor + "$");
+                System.out.print("Ingrese el dinero: ");
+                valor -= in.nextInt();
+
+                while (valor > 0) {
+                    System.out.print("Falta $" + valor + ", inregese el valor faltante: ");
+                    valor -= in.nextInt();
+                }
+
+                System.out.println("Pago confirmado.");
+                System.out.println("\nLe queda $" + (-1 * valor) + " a favor en la cuenta.");
+                billetera.depositar(valor * -1);
+            }
+
+            // Actualizar la billetera
+            billeteras.actualizar_billetera(billetera);
+
+            // Liberar el parqueadero y finalizar el servicio
+            entrada_salida.add_salida(usuario.get_id());
+            System.out.println("Vuelva pronto!");
         }
 
         in.close();
